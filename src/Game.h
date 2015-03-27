822001a1a13a1cbd936e5b9ad7e6c4947eb6e90c
Will Mayner <wmayner@gmail.com>
2015-03-27 15:49:32 -0500

    Code style

--- src/Game.h
+++ src/Game.h
@@ -7,16 +7,18 @@
  *
  */
 
-#ifndef _Game_h_included_
-#define _Game_h_included_
+#ifndef SRC_GAME_H_
+#define SRC_GAME_H_
 
-#include "globalConst.h"
-#include "Agent.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <vector>
 #include <map>
 #include <set>
-#include <stdio.h>
-#include <stdlib.h>
+
+#include "globalConst.h"
+#include "Agent.h"
 
 using namespace std;
 
@@ -25,32 +27,36 @@ using namespace std;
 #define startMazes 1
 #define cPI 3.14159265
 
-class Game{
-public:
+class Game {
+ public:
     vector<int> patterns;
-	void executeGame(Agent* agent,FILE *f,double sensorNoise,int repeat);
-	Game(char* filename);
-	~Game();
-	double mutualInformation(vector<int> A,vector<int>B);
-	double ei(vector<int> A,vector<int> B,int theMask);
-	double computeAtomicPhi(vector<int>A,int states);
-	double predictiveI(vector<int>A);
-	double nonPredictiveI(vector<int>A);
-	double predictNextInput(vector<int>A);
-	double computeR(vector<vector<int> > table,int howFarBack);
-	double computeOldR(vector<vector<int> > table);
-	double entropy(vector<int> list);
+    void executeGame(Agent* agent, FILE *f, double sensorNoise, int repeat);
+    explicit Game(char* filename);
+    ~Game();
+    double mutualInformation(vector<int> A, vector<int> B);
+    double ei(vector<int> A, vector<int> B, int theMask);
+    double computeAtomicPhi(vector<int> A, int states);
+    double predictiveI(vector<int> A);
+    double nonPredictiveI(vector<int> A);
+    double predictNextInput(vector<int> A);
+    double computeR(vector< vector<int> > table, int howFarBack);
+    double computeOldR(vector< vector<int> > table);
+    double entropy(vector<int> list);
 
-    vector<vector<int> > executeGameLogStates(Agent* agent,double sensorNoise);
-	void analyseKO(Agent* agent,int which, int setTo,double sensorNoise);
+    vector< vector<int> > executeGameLogStates(Agent* agent, double
+            sensorNoise);
+    void analyseKO(Agent* agent, int which, int setTo, double sensorNoise);
 
-    void represenationPerNodeSummary(Agent* agent,char* filename,double sensorNoise);
-    void makeFullAnalysis(Agent *agent,char *fileLead,double sensorNoise);
-    void makeSingleAgentAnalysis(Agent *agent,char *fileLead,int agent_num);
-    double computeRGiven(vector<int>W,vector<int>S,vector<int>B,int nrWstates,int nrSstates,int nrBstates);
-    void applyNoise(Agent *agent,double sensorNoise);
+    void represenationPerNodeSummary(Agent* agent, char* filename, double
+            sensorNoise);
+    void makeFullAnalysis(Agent *agent, char *fileLead, double sensorNoise);
+    void makeSingleAgentAnalysis(Agent *agent, char *fileLead, int agent_num);
+    double computeRGiven(vector<int> W, vector<int> S, vector<int> B, int
+            nrWstates, int nrSstates, int nrBstates);
+    void applyNoise(Agent *agent, double sensorNoise);
     double agentDependentRandDouble(void);
     int agentDependentRandInt(void);
     int nowUpdate;
 };
-#endif
+
+#endif  // SRC_GAME_H_
