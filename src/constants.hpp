0212bfc86a9e01f146479fdc32dd9851a48dcb90
Will Mayner <wmayner@gmail.com>
2015-04-01 11:25:58 -0500

    Categorize constants

--- src/constants.hpp
+++ src/constants.hpp
@@ -5,10 +5,17 @@
 
 #define VIRTUALENV_PATH "/Users/will/.virtualenvs/matching/lib/python3.4/site-packages"
 
+// Agent parameters
 #define NUM_NODES 8
+
+// World parameters
 #define WORLD_HEIGHT 36
 #define WORLD_WIDTH 20
+
+// Evolution parameters
 #define FITNESS_MULTIPLIER 1.01
+
+// Analysis parameters
 #define LOD_RECORD_INTERVAL 7
 
 #endif  // _SRC_CONSTANTS_H_
