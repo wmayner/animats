3b1cdae49cb34a516cc5734a443736b7cb326557
Will Mayner <wmayner@gmail.com>
2015-05-12 23:58:04 -0500

    Update runall.nix

--- runall.nix
+++ runall.nix
@@ -4,5 +4,5 @@ cd results/current
 
 for ((seed=0; seed < 28; seed++))
 do
-  ../../build/main.o ../../parameters/basic0.txt "seed_${seed}_LOD.txt" "seed_${seed}_GEN.txt" "seed_${seed}" ${seed} 0 2>&1 | tee "seed_"$seed"_output.log" &
+  ../../build/main.o ../../parameters/basic0.txt "seed-${seed}_LOD.csv" "seed-${seed}_GEN.txt" "seed-${seed}" ${seed} 0 2>&1 | tee "seed-"$seed"_output.log" &
 done
