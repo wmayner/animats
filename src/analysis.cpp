89bebce63d5b9398b887a2a0234cf8db9dd21171
Will Mayner <wmayner@gmail.com>
2015-05-13 15:42:23 -0500

    Make all data files CSV formatted

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -22,35 +22,26 @@ void makeFullAnalysis(Game *game, Agent *agent, char *fileLead, double
         agent = lineage[gen];
         if ((agent->born % LOD_RECORD_INTERVAL) == 0) {
             // TPM transition list
-            sprintf(filename, "%s_gen-%i_tpm.txt", fileLead, agent->born);
+            sprintf(filename, "%s_gen-%i_tpm.csv", fileLead, agent->born);
             f = fopen(filename, "w+t");
-            agent->saveLogicTable(f);
-            fclose(f);
+            agent->saveTPM(f);
             // Lifetime transition list
-            sprintf(filename, "%s_gen-%i_transitions.txt", fileLead, agent->born);
+            sprintf(filename, "%s_gen-%i_transitions.csv", fileLead, agent->born);
             f = fopen(filename, "w+t");
+            fprintf(f, "past_state,current_state\n");
             table = game->executeGame(agent, sensorNoise);
-            for (i = 0; i < 8; i++) {
-                fprintf(f, "past_state_%i,", i);
-            }
-            fprintf(f, ",");
-            for (i = 0; i < 8; i++) {
-                fprintf(f, "current_state_%i,", i);
-            }
-            fprintf(f, "\n");
             for (j = 0; j < (int)table[0].size(); j++)  {
-                for (i = 0; i < 8; i++)  {
-                    fprintf(f, "%i,", (table[0][j] >> i) & 1);
-                }
-                fprintf(f, ",");
-                for (i = 0; i < 8; i++) {
-                    fprintf(f, "%i,", (table[1][j] >> i) & 1);
+                for (i = 0; i < NUM_NODES - 1; i++)
+                    fprintf(f, "%i ", (table[0][j] >> i) & 1);
+                fprintf(f, "%i,", (table[0][j] >> i) & 1);
+                for (i = 0; i < NUM_NODES; i++) {
+                    fprintf(f, "%i ", (table[1][j] >> i) & 1);
                 }
                 fprintf(f, "\n");
             }
             fclose(f);
             // Edge list
-            sprintf(filename, "%s_gen-%i_edges.txt", fileLead, agent->born);
+            sprintf(filename, "%s_gen-%i_edges.csv", fileLead, agent->born);
             agent->saveEdgeList(filename);
         }
         agent = agent->ancestor;
@@ -60,24 +51,23 @@ void makeFullAnalysis(Game *game, Agent *agent, char *fileLead, double
 void makeSingleAgentAnalysis(Agent *agent, char *fileLead, int agent_num) {
     char filename[1000];
     FILE *f;
-    // TPM transition list
-    sprintf(filename, "%s_%i_%i_FullLogicTable.txt", fileLead, agent->born,
-            agent_num);
+    // TPM
+    sprintf(filename, "%s_gen-%i_ind-%i_tpm.csv", fileLead, agent->born,
+        agent_num);
     f = fopen(filename, "w+t");
-    agent->saveLogicTableSingleAnimat(f);
-    fclose(f);
-    // Fitness value
-    sprintf(filename, "%s_%i_Fitness.txt", fileLead, agent->born);
+    agent->saveTPM(f);
+    // Number correct
+    sprintf(filename, "%s_gent-%i_ind-%i_fitness.csv", fileLead, agent->born,
+        agent_num);
     if (agent_num == 0) {
         f = fopen(filename, "w+t");
     } else {
         f = fopen(filename, "a");
     }
-    fprintf(f, "%i", agent->correct);
-    fprintf(f, "\n");
+    fprintf(f, "correct\n%i\n", agent->correct);
     fclose(f);
     // Edge list
-    sprintf(filename, "%s_%i_%i_EdgeList.txt", fileLead, agent->born,
-            agent_num);
+    sprintf(filename, "%s_gen-%i_ind-%i_edges.csv", fileLead, agent->born,
+        agent_num);
     agent->saveEdgeList(filename);
 }
