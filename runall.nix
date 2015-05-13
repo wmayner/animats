e3a4931ad414a1a78ac7f275ce65ecdafcd8d702
Will Mayner <wmayner@gmail.com>
2015-05-13 00:00:38 -0500

    Make before running

--- runall.nix
+++ runall.nix
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+make
 cd results/current
 
-for ((seed=0; seed < 28; seed++))
+for ((seed=0; seed < 1; seed++))
 do
   ../../build/main.o ../../parameters/basic0.txt "seed-${seed}_LOD.csv" "seed-${seed}_GEN.txt" "seed-${seed}" ${seed} 0 2>&1 | tee "seed-"$seed"_output.log" &
 done
