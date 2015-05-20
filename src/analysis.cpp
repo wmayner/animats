1492039b4cec5157783ad721d2aff1f5021fd09b
Will Mayner <wmayner@gmail.com>
2015-05-20 18:06:01 -0500

    Don't prepend underscore for animat filenames

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -22,11 +22,11 @@ void makeFullAnalysis(Game *game, Agent *agent, char *fileLead, double
         agent = lineage[gen];
         if ((agent->born % LOD_RECORD_INTERVAL) == 0) {
             // TPM transition list
-            sprintf(filename, "%s_gen-%i_tpm.csv", fileLead, agent->born);
+            sprintf(filename, "%sgen-%i_tpm.csv", fileLead, agent->born);
             f = fopen(filename, "w+t");
             agent->saveTPM(f);
             // Lifetime transition list
-            sprintf(filename, "%s_gen-%i_transitions.csv", fileLead, agent->born);
+            sprintf(filename, "%sgen-%i_transitions.csv", fileLead, agent->born);
             f = fopen(filename, "w+t");
             fprintf(f, "past_state,current_state\n");
             table = game->executeGame(agent, sensorNoise);
@@ -41,7 +41,7 @@ void makeFullAnalysis(Game *game, Agent *agent, char *fileLead, double
             }
             fclose(f);
             // Edge list
-            sprintf(filename, "%s_gen-%i_edges.csv", fileLead, agent->born);
+            sprintf(filename, "%sgen-%i_edges.csv", fileLead, agent->born);
             agent->saveEdgeList(filename);
         }
         agent = agent->ancestor;
@@ -52,12 +52,12 @@ void makeSingleAgentAnalysis(Agent *agent, char *fileLead, int agent_num) {
     char filename[1000];
     FILE *f;
     // TPM
-    sprintf(filename, "%s_gen-%i_ind-%i_tpm.csv", fileLead, agent->born,
+    sprintf(filename, "%sgen-%i_ind-%i_tpm.csv", fileLead, agent->born,
         agent_num);
     f = fopen(filename, "w+t");
     agent->saveTPM(f);
     // Number correct
-    sprintf(filename, "%s_gent-%i_ind-%i_fitness.csv", fileLead, agent->born,
+    sprintf(filename, "%sgen-%i_ind-%i_fitness.csv", fileLead, agent->born,
         agent_num);
     if (agent_num == 0) {
         f = fopen(filename, "w+t");
@@ -67,7 +67,7 @@ void makeSingleAgentAnalysis(Agent *agent, char *fileLead, int agent_num) {
     fprintf(f, "correct\n%i\n", agent->correct);
     fclose(f);
     // Edge list
-    sprintf(filename, "%s_gen-%i_ind-%i_edges.csv", fileLead, agent->born,
+    sprintf(filename, "%sgen-%i_ind-%i_edges.csv", fileLead, agent->born,
         agent_num);
     agent->saveEdgeList(filename);
 }
