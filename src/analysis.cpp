bcd0ae437f64562884d9b707dc598e03fcfdcd28
Will Mayner <wmayner@gmail.com>
2015-05-22 13:18:23 -0500

    Clean up makeFullAnalysis a bit

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -4,47 +4,48 @@
 
 #include "./analysis.hpp"
 
-
-void makeFullAnalysis(Game *game, Agent *agent, char *fileLead, double
+void makeFullAnalysis(Game *game, Agent *finalAgent, char *fileLead, double
         sensorNoise) {
     char filename[1000];
     FILE *f;
-    int i, j;
     vector< vector<int> > table;
 
     vector<Agent*> lineage;
-    Agent *localAgent = agent;
-    while (localAgent != NULL) {
-        lineage.push_back(localAgent);
-        localAgent = localAgent->ancestor;
+    Agent *agent = finalAgent;
+    while (agent != NULL) {
+        lineage.push_back(agent);
+        agent = agent->ancestor;
     }
-    for (int gen = 0; gen < (int)lineage.size(); gen++) {
-        agent = lineage[gen];
+    for (int i = 0; i < (int)lineage.size(); i++) {
+        agent = lineage[i];
         if ((agent->born % LOD_RECORD_INTERVAL) == 0) {
             // TPM transition list
             sprintf(filename, "%sgen-%i_tpm.csv", fileLead, agent->born);
             f = fopen(filename, "w+t");
             agent->saveTPM(f);
+
             // Lifetime transition list
             sprintf(filename, "%sgen-%i_transitions.csv", fileLead, agent->born);
             f = fopen(filename, "w+t");
             fprintf(f, "past_state,current_state\n");
+
             table = game->executeGame(agent, sensorNoise);
-            for (j = 0; j < (int)table[0].size(); j++)  {
-                for (i = 0; i < NUM_NODES - 1; i++)
-                    fprintf(f, "%i", (table[0][j] >> i) & 1);
-                fprintf(f, "%i,", (table[0][j] >> i) & 1);
-                for (i = 0; i < NUM_NODES; i++) {
-                    fprintf(f, "%i", (table[1][j] >> i) & 1);
+            for (int j = 0; j < (int)table[0].size(); j++)  {
+                for (int k = 0; k < NUM_NODES - 1; k++) {
+                    fprintf(f, "%i", (table[0][j] >> k) & 1);
+                }
+                fprintf(f, "%i,", (table[0][j] >> (NUM_NODES - 1)) & 1);
+                for (int k = 0; k < NUM_NODES; k++) {
+                    fprintf(f, "%i", (table[1][j] >> k) & 1);
                 }
                 fprintf(f, "\n");
             }
             fclose(f);
+
             // Edge list
             sprintf(filename, "%sgen-%i_edges.csv", fileLead, agent->born);
             agent->saveEdgeList(filename);
         }
-        agent = agent->ancestor;
     }
 }
 
