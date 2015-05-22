956147d4b5d2ff0c0d643ff597744321d9836d60
Will Mayner <wmayner@gmail.com>
2015-05-22 13:20:35 -0500

    Select first, then evaluate
    - Fixed dangling pointer bug introduced in an attempt to fix how LODs
      are saved (first and last generation are now properly recorded).
    - Clean up LOD-saving function.

--- src/main.cpp
+++ src/main.cpp
@@ -23,64 +23,92 @@ using std::map;
 using std::endl;
 using std::cout;
 
-int generation = 0;
 int repeats = 1;
 double sensorNoise = 0.0;
 
 void saveLODandGenomes(Agent *agent, FILE *statsFile, FILE *genomeFile);
 
 int main(int argc, char *argv[]) {
-    vector<Agent*> agent;
-    vector<Agent*> nextGen;
-    Agent *masterAgent;
-    int i, j, who = 0;
     Game *game;
     double maxFitness;
-    FILE *LODFile;
-    FILE *genomeFile;
-    LODFile = fopen(argv[2], "w+t");
-    genomeFile = fopen(argv[3], "w+t");
+    FILE *LODFile = fopen(argv[2], "w+t");
+    FILE *genomeFile = fopen(argv[3], "w+t");
+
     int localSeed = atoi(argv[5]);
     if (localSeed != -1) {
         srand(localSeed);
     } else {
         srand(getpid());
     }
+
+    // Population and offspring containers.
+    vector<Agent*> agent;
     agent.resize(NUM_AGENTS);
+    vector<Agent*> nextGen;
     nextGen.resize(agent.size());
 
     game = new Game(argv[1]);
 
     sensorNoise = atof(argv[6]);
 
-    masterAgent = new Agent;
+    // Prototypical Agent from which all descend.
+    Agent *masterAgent = new Agent;
     masterAgent->setupEmptyAgent(5000);
     masterAgent->setupPhenotype();
-    for (i = 0; i < (int)agent.size(); i++) {
+    masterAgent->born = 0;
+
+    // Agent whose line of descent will be traced in the analysis.
+    Agent *bestAgent = masterAgent;
+
+    int generation = 1;
+    // Make the initial population.
+    for (int i = 0; i < (int)agent.size(); i++) {
         agent[i] = new Agent;
         agent[i]->inherit(masterAgent, generation);
     }
 
-    cout << "Setup complete." << endl;
-
     int startTime = time(NULL);
-    while (generation < NUM_GENERATIONS) {
-        for (i = 0; i < (int)agent.size(); i++) {
+
+    int who;
+    while (generation < NUM_GENERATIONS + 1) {
+        // Variant of roulette-wheel selection that uses the maximum fitness
+        // rather than the average.
+        // See http://en.wikipedia.org/wiki/Fitness_proportionate_selection
+        for (int i = 0; i < (int)agent.size(); i++) {
+            Agent *d;
+            d = new Agent;
+            int j;
+            do {
+                j = rand() % (int)agent.size();
+            } while (randDouble > (agent[j]->fitness / maxFitness));
+            d->inherit(agent[j], generation);
+            nextGen[i] = d;
+        }
+        // Delete lines of descent that died out.
+        for (int i = 0; i < (int)agent.size(); i++) {
+            agent[i]->nrPointingAtMe--;
+            if (agent[i]->nrPointingAtMe == 0)
+                delete agent[i];
+        }
+        // Replace current population with offspring.
+        agent = nextGen;
+
+
+        // Fitness evaluation.
+        for (int i = 0; i < (int)agent.size(); i++) {
             agent[i]->fitness = 0.0;
             agent[i]->fitnesses.clear();
         }
-        for (i = 0; i < (int)agent.size(); i++) {
-            for (j = 0; j < repeats; j++) {
+        for (int i = 0; i < (int)agent.size(); i++) {
+            for (int j = 0; j < repeats; j++) {
                 game->executeGame(agent[i], sensorNoise, j);
                 agent[i]->fitnesses.push_back((float)agent[i]->correct);
             }
         }
-
         maxFitness = 0.0;
-
-        for (i = 0; i < (int)agent.size(); i++) {
+        for (int i = 0; i < (int)agent.size(); i++) {
             agent[i]->fitness = 1.0;
-            for (j = 0; j < repeats; j++)
+            for (int j = 0; j < repeats; j++)
                 agent[i]->fitness *= agent[i]->fitnesses[j];
             if (repeats <= 1) {
               // Larissa: This for one repeat
@@ -95,50 +123,34 @@ int main(int argc, char *argv[]) {
             }
         }
 
+        bestAgent = agent[who];
+        generation++;
+
         if (generation % PRINT_INTERVAL == 0) {
           cout << "Generation " << generation << ": [fitness] " <<
             (double)maxFitness << " [correct/incorrect] " <<
-            agent[who]->correct << "/" << agent[who]->incorrect << endl;
+            bestAgent->correct << "/" << bestAgent->incorrect << endl;
         }
-
-        generation++;
-
-        // Variant of roulette-wheel selection that uses the maximum fitness
-        // rather than the average.
-        // See http://en.wikipedia.org/wiki/Fitness_proportionate_selection
-        for (i = 0; i < (int)agent.size(); i++) {
-            Agent *d;
-            d = new Agent;
-            do {
-                j = rand() % (int)agent.size();
-            } while (randDouble > (agent[j]->fitness / maxFitness));
-            d->inherit(agent[j], generation);
-            nextGen[i] = d;
-        }
-        // Delete lines of descent that died out.
-        for (i = 0; i < (int)agent.size(); i++) {
-            agent[i]->nrPointingAtMe--;
-            if (agent[i]->nrPointingAtMe == 0)
-                delete agent[i];
-        }
-        // Replace current population with offspring.
-        agent = nextGen;
     }
 
     int endTime = time(NULL);
     cout << "Finished simulating " << NUM_GENERATIONS << " generations. Elapsed time: " << (endTime - startTime) << " seconds." << endl;
     // Larissa: set sensor noise to 0 for analysis
-    makeFullAnalysis(game, agent[0], argv[4], 0);
-    saveLODandGenomes(agent[who], LODFile, genomeFile);
+    makeFullAnalysis(game, bestAgent, argv[4], 0);
+    cout << "Finished doing full analysis." << endl;
+    saveLODandGenomes(bestAgent, LODFile, genomeFile);
+    cout << "Saved LOD and genomes." << endl;
+    fclose(LODFile);
+    fclose(genomeFile);
     return 0;
 }
 
-void saveLODandGenomes(Agent *agent, FILE *LODFile, FILE *genomeFile) {
+void saveLODandGenomes(Agent *finalAgent, FILE *LODFile, FILE *genomeFile) {
     vector<Agent*> lineage;
-    Agent *localAgent = agent;
-    while (localAgent != NULL) {
-        lineage.push_back(localAgent);
-        localAgent = localAgent->ancestor;
+    Agent *agent = finalAgent;
+    while (agent != NULL) {
+        lineage.push_back(agent);
+        agent = agent->ancestor;
     }
     // Start with the most-evolved animat and trace backwards through the line
     // of descent, saving stats and genome every (LOD_RECORD_INTERVAL)th
@@ -153,15 +165,14 @@ void saveLODandGenomes(Agent *agent, FILE *LODFile, FILE *genomeFile) {
         fprintf(LODFile, ",correct_pattern_%i", i);
     }
     fprintf(LODFile, "\n");
-    // Skip final population whose fitness hasn't been evaluated
-    for (int gen = (int)lineage.size() - 2; gen >= 0; gen--) {
-        agent = lineage[gen];
+    for (int i = (int)lineage.size() - 1; i >= 0; i--) {
+        agent = lineage[i];
         if ((agent->born % LOD_RECORD_INTERVAL) == 0) {
             // Append data to LOD file
             fprintf(LODFile, "%i,%i,%i", agent->born, agent->correct,
                 agent->incorrect);
-            for (int i = 0; i < (int)agent->numCorrectByPattern.size(); i++) {
-                fprintf(LODFile, ",%i", agent->numCorrectByPattern[i]);
+            for (int j = 0; j < (int)agent->numCorrectByPattern.size(); j++) {
+                fprintf(LODFile, ",%i", agent->numCorrectByPattern[j]);
             }
             fprintf(LODFile, "\n");
             // Record genome
