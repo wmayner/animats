27cee8475ce76eb1c3724b3bc443d6ebd886943f
Will Mayner <wmayner@gmail.com>
2015-05-26 14:24:15 -0500

    Separate raw and compiled results into directories

--- analyze.py
+++ analyze.py
@@ -2,21 +2,32 @@
 # -*- coding: utf-8 -*-
 # analyze.py
 
+import os
 import pickle
 from glob import glob
 import numpy as np
 
-lod_files = (glob('results/current/*/LOD.csv'))
 
-lods = {}
-for f in lod_files:
-    print('Processing ' + f + '...')
-    lods[f] = np.genfromtxt(f, delimiter=',', dtype=int, skip_header=1)
+RESULT_DIR = os.path.join('raw_results', '3-4-6-5')
+LOD_FILES = glob(os.path.join(RESULT_DIR, '*/LOD.csv'))
 
-# Get the number of correct trials at the last generation.
-correct = np.array([lods[f][(-1, 1)] for f in lods])
+CORRECT_COUNTS_FILEPATH = os.path.join('compiled_results',
+                                       'correct_counts.pkl')
 
-DATA_FILE = 'data.pkl'
-with open(DATA_FILE, 'wb') as f:
-    pickle.dump(correct, f)
-    print('Saved analysis to `{}`.'.format(DATA_FILE))
+
+def save_correct_counts(output_filepath=CORRECT_COUNTS_FILEPATH):
+    lods = {}
+    for f in LOD_FILES:
+        print('Processing ' + f + '...')
+        lods[f] = np.genfromtxt(f, delimiter=',', dtype=int, skip_header=1)
+    # Get the number of correct trials at the last generation.
+    correct = np.array([lods[f][(-1, 1)] for f in lods])
+    with open(output_filepath, 'wb') as f:
+        pickle.dump(correct, f)
+        print('Saved analysis to `{}`.'.format(output_filepath))
+    return correct
+
+
+def load_correct_counts(input_filepath=CORRECT_COUNTS_FILEPATH):
+    with open(input_filepath, 'rb') as f:
+        return pickle.load(f)
