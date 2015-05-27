93541c28d0492e4dd19e1b5a175ecb63f6e4ae50
Will Mayner <wmayner@gmail.com>
2015-05-27 16:52:57 -0500

    Consolidate plot and analyze

--- analyze.py
+++ analyze.py
@@ -6,6 +6,7 @@ import os
 import pickle
 from glob import glob
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 RESULT_DIR = os.path.join('raw_results', '3-4-6-5')
@@ -19,15 +20,42 @@ def save_correct_counts(output_filepath=CORRECT_COUNTS_FILEPATH):
     lods = {}
     for f in LOD_FILES:
         print('Processing ' + f + '...')
-        lods[f] = np.genfromtxt(f, delimiter=',', dtype=int, skip_header=1)
+        try:
+            lods[f] = np.genfromtxt(f, delimiter=',', dtype=int, skip_header=1)
+        except StopIteration:
+            print('\tEmpty file: ' + f)
+    # Check that the same number of generations were used for every seed.
+    ngen = lods[list(lods.keys())[0]][-1][0]
+    assert all(ngen == lod[-1][0] for lod in lods.values())
     # Get the number of correct trials at the last generation.
-    correct = np.array([lods[f][(-1, 1)] for f in lods])
+    data = {
+        'correct': np.array([lod[(-1, 1)] for lod in lods.values()]),
+        'ngen': ngen
+    }
     with open(output_filepath, 'wb') as f:
-        pickle.dump(correct, f)
+        pickle.dump(data, f)
         print('Saved analysis to `{}`.'.format(output_filepath))
-    return correct
+    return data
 
 
 def load_correct_counts(input_filepath=CORRECT_COUNTS_FILEPATH):
     with open(input_filepath, 'rb') as f:
         return pickle.load(f)
+
+
+def plot_correct_counts(input_filepath=CORRECT_COUNTS_FILEPATH):
+    with open(input_filepath, 'rb') as f:
+        data = pickle.load(f)
+    correct, ngen = data['correct'], data['ngen']
+    n, bins, patches = plt.hist(correct, 20, normed=True, facecolor='blue',
+                                alpha=0.8)
+    plt.xlabel('$\mathrm{Fitness}$')
+    plt.ylabel('$\mathrm{Number\ of\ Animats}$')
+    plt.title('$\mathrm{Histogram\ of\ Animat\ Performance\ after\ '
+              + str(ngen) + '\ generations,\ population\ size\ 100}$')
+    plt.grid(True)
+
+    plt.show()
+
+
+def plot_lods(input_filepaths=LOD_FILES)
