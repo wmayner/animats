100889c26637da9e5828c2c4399085d92468403a
Will Mayner <wmayner@gmail.com>
2015-05-28 15:37:57 -0500

    Add `plot_lod` function

--- analyze.py
+++ analyze.py
@@ -9,27 +9,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-RESULT_DIR = os.path.join('raw_results', '3-4-6-5')
+RESULT_CATEGORY = '3-4-6-5'
+RESULT_DIR = os.path.join('raw_results', RESULT_CATEGORY)
 LOD_FILES = glob(os.path.join(RESULT_DIR, '*/LOD.csv'))
 
-CORRECT_COUNTS_FILEPATH = os.path.join('compiled_results',
+COMPILED_RESULT_DIR = os.path.join('compiled_results', '3-4-6-5')
+CORRECT_COUNTS_FILEPATH = os.path.join(COMPILED_RESULT_DIR,
                                        'correct_counts.pkl')
 
+if not os.path.exists(COMPILED_RESULT_DIR):
+    os.makedirs(COMPILED_RESULT_DIR)
 
-def save_correct_counts(output_filepath=CORRECT_COUNTS_FILEPATH):
+
+def load_lods(input_filepaths=LOD_FILES):
     lods = {}
     for f in LOD_FILES:
         print('Processing ' + f + '...')
         try:
-            lods[f] = np.genfromtxt(f, delimiter=',', dtype=int, skip_header=1)
+            lods[f] = np.genfromtxt(f, delimiter=',', dtype=int, names=True)
         except StopIteration:
             print('\tEmpty file: ' + f)
+    return lods
+
+
+def save_correct_counts(output_filepath=CORRECT_COUNTS_FILEPATH):
+    lods = load_lods()
     # Check that the same number of generations were used for every seed.
-    ngen = lods[list(lods.keys())[0]][-1][0]
+    ngen = lods[list(lods.keys())[0]]['gen'][-1]
     assert all(ngen == lod[-1][0] for lod in lods.values())
     # Get the number of correct trials at the last generation.
     data = {
-        'correct': np.array([lod[(-1, 1)] for lod in lods.values()]),
+        'correct': np.array([lod['correct'][-1] for lod in lods.values()]),
         'ngen': ngen
     }
     with open(output_filepath, 'wb') as f:
@@ -58,4 +68,15 @@ def plot_correct_counts(input_filepath=CORRECT_COUNTS_FILEPATH):
     plt.show()
 
 
-def plot_lods(input_filepaths=LOD_FILES)
+def plot_lods(input_filepaths=LOD_FILES):
+    lods = list(load_lods().values())
+    ngen = lods[0]['gen'][-1]
+    for lod in lods:
+        if lod['correct'][-1] == 64:
+            plt.plot(lod['gen'], lod['correct'])
+    plt.xlabel('$\mathrm{Generation}$', fontsize=22)
+    plt.ylabel('$\mathrm{Correct\ trials}$', fontsize=22)
+    plt.title('$\mathrm{Animat\ Fitness\ over\ ' + str(ngen) +
+              '\ generations,\ population\ size\ 100}$', fontsize=22)
+    plt.grid(True)
+    plt.show()
