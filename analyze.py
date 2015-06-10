38f3e927e6a8ea2bd07b1c789423afa4088dd7fe
Will Mayner <wmayner@gmail.com>
2015-06-10 11:41:32 -0500

    Update analyze

--- analyze.py
+++ analyze.py
@@ -14,22 +14,35 @@ RESULT_DIR = os.path.join('raw_results', RESULT_CATEGORY)
 LOD_FILES = glob(os.path.join(RESULT_DIR, '*/LOD.csv'))
 
 COMPILED_RESULT_DIR = os.path.join('compiled_results', '3-4-6-5')
+
 CORRECT_COUNTS_FILEPATH = os.path.join(COMPILED_RESULT_DIR,
                                        'correct_counts.pkl')
+
+COMPILED_LODS_FILEPATH = os.path.join(COMPILED_RESULT_DIR, 'lods.pkl')
 
 if not os.path.exists(COMPILED_RESULT_DIR):
     os.makedirs(COMPILED_RESULT_DIR)
 
 
-def load_lods(input_filepaths=LOD_FILES):
+def save_lods(input_filepaths=LOD_FILES,
+              output_filepath=COMPILED_LODS_FILEPATH):
     lods = {}
     for f in LOD_FILES:
         print('Processing ' + f + '...')
         try:
             lods[f] = np.genfromtxt(f, delimiter=',', dtype=int, names=True)
-        except StopIteration:
+        except (StopIteration, IndexError):
             print('\tEmpty file: ' + f)
+    with open(output_filepath, 'wb') as f:
+        pickle.dump(lods, f)
     return lods
+
+
+def load_lods(input_filepath=COMPILED_LODS_FILEPATH):
+    if os.path.exists(input_filepath):
+        with open(input_filepath, 'rb') as f:
+            return pickle.load(f)
+    return save_lods(input_filepath)
 
 
 def save_correct_counts(output_filepath=CORRECT_COUNTS_FILEPATH):
@@ -46,6 +59,24 @@ def save_correct_counts(output_filepath=CORRECT_COUNTS_FILEPATH):
         pickle.dump(data, f)
         print('Saved analysis to `{}`.'.format(output_filepath))
     return data
+
+
+def plot_lods(input_filepaths=LOD_FILES, avg=True):
+    lods = list(load_lods().values())
+    ngen = lods[0]['gen'][-1]
+    data = np.array([lod['correct'] for lod in lods])
+    if avg:
+        plt.plot(lods[0]['gen'], data.mean(0))
+    else:
+        for lod in lods:
+            if lod['correct'][-1] == 64:
+                plt.plot(lod['gen'], lod['correct'])
+    plt.xlabel('$\mathrm{Generation}$', fontsize=22)
+    plt.ylabel('$\mathrm{Correct\ trials}$', fontsize=22)
+    plt.title('$\mathrm{Animat\ Fitness\ over\ ' + str(ngen) +
+              '\ generations,\ population\ size\ 100}$', fontsize=22)
+    plt.grid(True)
+    plt.show()
 
 
 def load_correct_counts(input_filepath=CORRECT_COUNTS_FILEPATH):
@@ -66,18 +97,4 @@ def plot_correct_counts(input_filepath=CORRECT_COUNTS_FILEPATH):
               fontsize=22)
     plt.grid(True)
 
-    plt.show()
-
-
-def plot_lods(input_filepaths=LOD_FILES):
-    lods = list(load_lods().values())
-    ngen = lods[0]['gen'][-1]
-    for lod in lods:
-        if lod['correct'][-1] == 64:
-            plt.plot(lod['gen'], lod['correct'])
-    plt.xlabel('$\mathrm{Generation}$', fontsize=22)
-    plt.ylabel('$\mathrm{Correct\ trials}$', fontsize=22)
-    plt.title('$\mathrm{Animat\ Fitness\ over\ ' + str(ngen) +
-              '\ generations,\ population\ size\ 100}$', fontsize=22)
-    plt.grid(True)
     plt.show()
